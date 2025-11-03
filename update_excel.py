9634386e448c12229666716c54d549cea56b6fc2
Lokeswar Reddy <105974270+Theblr7@users.noreply.github.com>
2025-11-03 17:30:24 +0530

    Update update_excel.py

--- update_excel.py
+++ update_excel.py
@@ -1,19 +1,19 @@
 import json
 import openpyxl
-from datetime import datetime
 
 def update_excel(entry):
     wb = openpyxl.load_workbook('SpendTracker.xlsx')
     sheet = wb['DailySpend']
     sheet.append([
         entry['date'],
+        entry['creditCard'],
         entry['category'],
         float(entry['amount']),
-        entry.get('notes', '')
+        entry.get('notes', ''),
+        float(entry['cashback'])
     ])
     wb.save('SpendTracker.xlsx')
 
-# Example usage
 with open('entry.json') as f:
     data = json.load(f)
     update_excel(data)
